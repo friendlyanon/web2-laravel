4bd2637ea1ea64f878dcf30783b53e6ca078d1ae
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-06 17:13:04 +0200

    Add Loop class to typehint loop variable in Blade files

---
+++ app/Utils/Loop.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Utils;
+
+/**
+ * @property-read int iteration
+ * @property-read int index
+ * @property-read null|int remaining
+ * @property-read null|int count
+ * @property-read boolean first
+ * @property-read null|boolean last
+ * @property-read boolean odd
+ * @property-read boolean even
+ * @property-read int depth
+ * @property-read null|Loop parent
+ */
+final class Loop
+{
+    private function __construct() { }
+}
