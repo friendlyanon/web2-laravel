eca4ae8cfbc5b1c8e25edd79e44cbe32db0fda76
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-07 13:17:06 +0200

    Null should be the last type

--- app/Utils/Loop.php
+++ app/Utils/Loop.php
@@ -5,14 +5,14 @@ namespace App\Utils;
 /**
  * @property-read int iteration
  * @property-read int index
- * @property-read null|int remaining
- * @property-read null|int count
+ * @property-read int|null remaining
+ * @property-read int|null count
  * @property-read boolean first
- * @property-read null|boolean last
+ * @property-read boolean|null last
  * @property-read boolean odd
  * @property-read boolean even
  * @property-read int depth
- * @property-read null|Loop parent
+ * @property-read Loop|null parent
  */
 final class Loop
 {
