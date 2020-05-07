ccee5a2ce81cd05af7d4e235f5533643f8ad4e43
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-07 14:06:23 +0200

    Add foreign keys

---
+++ database/migrations/2020_05_07_113253_add_foreign_key_to_tables.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('partners', static function (Blueprint $table) {
+            $table->foreignId('partner_group_id')->constrained();
+        });
+
+        Schema::table('products', static function (Blueprint $table) {
+            $table->foreignId('tax_id')->constrained();
+        });
+
+        Schema::table('invoices', static function (Blueprint $table) {
+            $table->foreignId('partner_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('partners', static function (Blueprint $table) {
+            $table->dropForeign('partner_group_id');
+        });
+
+        Schema::table('products', static function (Blueprint $table) {
+            $table->dropForeign('tax_id');
+        });
+
+        Schema::table('invoices', static function (Blueprint $table) {
+            $table->dropForeign('partner_id');
+            $table->dropForeign('product_id');
+        });
+    }
+}
