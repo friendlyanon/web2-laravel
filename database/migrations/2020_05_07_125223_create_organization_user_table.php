189d69947509816692d8f4de7e71981bf8ba3a4e
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-07 15:13:10 +0200

    Create pivot for organization and user relation

---
+++ database/migrations/2020_05_07_125223_create_organization_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_user', static function (Blueprint $table) {
+            $table->foreignId('organization_id')->constrained()->onDelete('CASCADE');
+            $table->foreignId('user_id')->constrained()->onDelete('CASCADE');
+            $table->timestamps();
+            $table->unique(['organization_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization_user');
+    }
+}
