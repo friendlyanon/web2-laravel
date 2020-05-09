cc51d69291cdc0bc629514e82d88cb7b3afadeb3
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-09 18:39:04 +0200

    Add user error strings

---
+++ resources/lang/hu/error.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'user' => [
+        'destroy' => 'Hiba történt a felhasználó törlése közben.',
+        'store' => 'Hiba történt a felhasználó mentése közben.',
+        'update' => 'Hiba történt a felhasználó frissítése közben.',
+    ],
+];
