573e5850599b1c16ee3d561a11254f27583cac37
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-09 13:20:18 +0200

    Use hashed password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use App\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -45,7 +46,7 @@ class CreateUsersTable extends Migration
         User::create([
             'name' => 'admin',
             'email' => 'admin@domain.tld',
-            'password' => 'admin',
+            'password' => Hash::make('admin'),
             'is_admin' => true,
         ]);
     }
