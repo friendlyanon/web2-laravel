91d5745a9acaf1dfce530cfe2874ce342d09b48d
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-11 13:22:03 +0200

    Hashing is unnecessary with the mutator in the User model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,6 @@
 use App\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -46,7 +45,7 @@ class CreateUsersTable extends Migration
         User::create([
             'name' => 'admin',
             'email' => 'admin@domain.tld',
-            'password' => Hash::make('admin'),
+            'password' => 'admin',
             'is_admin' => true,
         ]);
     }
