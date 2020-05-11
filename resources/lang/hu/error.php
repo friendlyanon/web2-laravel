e1ca74d22e140e86d03e94bf56126898cb1272e3
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-11 17:43:15 +0200

    Add error strings

--- resources/lang/hu/error.php
+++ resources/lang/hu/error.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'api' => [
+        'unknown' => 'Ismeretlen keresési feltétel.',
+    ],
     'users' => [
         'destroy' => 'Hiba történt a felhasználó törlése közben.',
         'store' => 'Hiba történt a felhasználó mentése közben.',
@@ -10,5 +13,36 @@ return [
         'destroy' => 'Hiba történt a vállalkozás törlése közben.',
         'store' => 'Hiba történt a vállalkozás mentése közben.',
         'update' => 'Hiba történt a vállalkozás frissítése közben.',
+    ],
+    'discounts' => [
+        'destroy' => 'Hiba történt a kedvezmény törlése közben.',
+        'store' => 'Hiba történt a kedvezmény mentése közben.',
+        'update' => 'Hiba történt a kedvezmény frissítése közben.',
+    ],
+    'invoices' => [
+        'destroy' => 'Hiba történt a számla törlése közben.',
+        'store' => 'Hiba történt a számla mentése közben.',
+        'update' => 'Hiba történt a számla frissítése közben.',
+        'editable' => 'Ez a számla már nem szerkeszthető.',
+    ],
+    'partners' => [
+        'destroy' => 'Hiba történt a partner törlése közben.',
+        'store' => 'Hiba történt a partner mentése közben.',
+        'update' => 'Hiba történt a partner frissítése közben.',
+    ],
+    'partner_groups' => [
+        'destroy' => 'Hiba történt a partnercsoport törlése közben.',
+        'store' => 'Hiba történt a partnercsoport mentése közben.',
+        'update' => 'Hiba történt a partnercsoport frissítése közben.',
+    ],
+    'products' => [
+        'destroy' => 'Hiba történt a termék törlése közben.',
+        'store' => 'Hiba történt a termék mentése közben.',
+        'update' => 'Hiba történt a termék frissítése közben.',
+    ],
+    'taxes' => [
+        'destroy' => 'Hiba történt az adó törlése közben.',
+        'store' => 'Hiba történt az adó mentése közben.',
+        'update' => 'Hiba történt az adó frissítése közben.',
     ],
 ];
