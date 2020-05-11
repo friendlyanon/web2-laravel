e3081a2d5a4db9738b05a4e603a6e031c433f1e8
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-05-11 16:50:06 +0200

    Only open invoices are editable

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\InvoiceRequest;
 use App\Invoice;
+use Lang;
+use RuntimeException;
 
 class InvoiceController extends SubResourceController
 {
     protected $model = Invoice::class;
 
     protected $request = InvoiceRequest::class;
+
+    /** @param Invoice $model */
+    protected function validateEditable($model)
+    {
+        if ($model->status !== Invoice::STATUS_OPEN) {
+            throw new RuntimeException(Lang::get('error.invoices.editable'));
+        }
+    }
 }
